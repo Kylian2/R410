716cd7c6b3a93728a9dfcbbc2bcaf2b5286ce018
Kylian2 <kylian.richard@gmail.com>
2025-01-29 11:49:05 +0100

    TP1: ex3

--- TP1/ex3/script/index.js
+++ TP1/ex3/script/index.js
@@ -10,3 +10,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
     table.addEventListener('click', change_color);
 })
 
+function ajouter_deux_couleurs(){
+    const color1 = document.getElementById('color-1').value;
+    const color2 = document.getElementById('color-2').value;
+    const td1 = document.createElement("td");
+    td1.appendChild(document.createTextNode(color1));
+    td1.style.background = color1;
+    const td2 = document.createElement("td");
+    td2.appendChild(document.createTextNode(color2));
+    td2.style.background = color2;
+    const tr = document.createElement('tr');
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+
+    const table = document.querySelector('table');
+    table.appendChild(tr);
+}
