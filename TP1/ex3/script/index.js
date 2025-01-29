e7c141acb0955a464555165e6d56cd75ec02e06e
Kylian2 <kylian.richard@gmail.com>
2025-01-29 12:03:26 +0100

    TP1: ex3 bonus"

--- TP1/ex3/script/index.js
+++ TP1/ex3/script/index.js
@@ -1,28 +1,20 @@
 function change_color(event) {
-    const color = event.target.innerText;
+    const color = event.target.getAttribute('data-color');
     const paragrahes = document.querySelectorAll("aside > p");
     paragrahes.forEach((p) => p.style.color = color);
 }
 
 document.addEventListener("DOMContentLoaded", (event) => { 
-    const table = document.querySelector('table');
-
-    table.addEventListener('click', change_color);
+    const colors = document.getElementById('color');
+    colors.addEventListener('click', change_color);
 })
 
 function ajouter_deux_couleurs(){
     const color1 = document.getElementById('color-1').value;
-    const color2 = document.getElementById('color-2').value;
-    const td1 = document.createElement("td");
-    td1.appendChild(document.createTextNode(color1));
-    td1.style.background = color1;
-    const td2 = document.createElement("td");
-    td2.appendChild(document.createTextNode(color2));
-    td2.style.background = color2;
-    const tr = document.createElement('tr');
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-
-    const table = document.querySelector('table');
-    table.appendChild(tr);
+    const color = document.createElement("span");
+    color.style.background = color1;
+    color.classList.add("colorbox");
+    color.setAttribute('data-color', color1);
+    const colors = document.getElementById('color');
+    colors.appendChild(color);
 }
