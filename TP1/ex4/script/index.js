a2c0a585ed2e0477008f29a9502f50811a5cd26c
Kylian2 <kylian.richard@gmail.com>
2025-02-05 11:16:36 +0100

    TP1: ex4

--- TP1/ex4/script/index.js
+++ TP1/ex4/script/index.js
@@ -7,9 +7,11 @@ function change_color(event) {
 document.addEventListener("DOMContentLoaded", (event) => { 
     const colors = document.getElementById('color');
     colors.addEventListener('click', change_color);
+    const paragrahes = document.querySelectorAll('main p');
+    paragrahes.forEach((p) => p.setAttribute('data-original', p.innerText));
 })
 
-function ajouter_deux_couleurs(){
+function ajouter_une_couleur(){
     const color1 = document.getElementById('color-1').value;
     const color = document.createElement("span");
     color.style.background = color1;
@@ -18,3 +20,45 @@ function ajouter_deux_couleurs(){
     const colors = document.getElementById('color');
     colors.appendChild(color);
 }
+
+function cherche_dans_paragraphes_de_main(event){
+    const search = document.getElementById("search");
+    const paragrahes = document.querySelectorAll("main p");
+    let find = false;
+    paragrahes.forEach((p) => {
+        if(p.innerText.includes(search.value) && search.value !== ''){
+            p.classList.add("highlight");
+            find = true;
+        }else{
+            p.classList.remove("highlight");
+        }
+    })
+    if(!find){
+        search.setCustomValidity("Inexistant");
+    }else{
+        search.setCustomValidity("");
+    }
+    search.reportValidity();
+}  
+
+
+function cherche_dans_paragraphes_de_main_v2(event){
+    const search = document.getElementById("search");
+    const paragrahes = document.querySelectorAll("main p");
+    let find = false;
+    paragrahes.forEach((p) => {
+        if(p.innerText.includes(search.value) && search.value !== ''){
+            let s = '<span class="highlight">'+search.value+'</span>';
+            p.innerHTML = p.getAttribute('data-original').replaceAll(search.value, s);
+            find = true;
+        }else{
+            p.innerHTML = p.getAttribute('data-original');
+        }
+    })
+    if(!find && search.value !== ''){
+        search.setCustomValidity("Inexistant");
+    }else{
+        search.setCustomValidity("");
+    }
+    search.reportValidity();
+}  
