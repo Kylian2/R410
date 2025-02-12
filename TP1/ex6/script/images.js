1236b522be81db8f128e3dc46fd630ed8fb604fb
Kylian2 <kylian.richard@gmail.com>
2025-02-12 11:16:22 +0100

    TP1: ex6

--- TP1/ex6/script/images.js
+++ TP1/ex6/script/images.js
@@ -15,6 +15,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
     f1.addEventListener('mousemove', move_img);
     f1.style.top = '1px';
     f1.style.left = '1px';
+
+    const pictures = document.querySelectorAll('.album > img');
+    pictures.forEach((p) => p.addEventListener('click', echange_images));
+    
+    const echange = document.getElementById('echange');
+    echange.addEventListener('change', () => {
+        if(!echange.checked && firstpic){
+            firstpic.style.border = '';
+            firstpic = undefined;
+        }
+    })
 })
 
 
@@ -23,5 +34,36 @@ function move_img(event){
         const element = event.target;
         element.style.left = (Number.parseInt(element.style.left) + event.movementX) + "px";
         element.style.top = (Number.parseInt(element.style.top) + event.movementY) + "px";
+    }
+}
+
+let firstpic = undefined;
+let secondpic = undefined;
+
+function echange_images(event){
+    const echange = document.getElementById('echange');
+    if(echange.checked){
+        if(!firstpic){
+            firstpic = event.target;
+            firstpic.style.border = '2px solid red';
+        }else{
+            secondpic = event.target;
+            
+            let parent = secondpic.parentNode;
+
+            for (let c of parent.childNodes) {
+                if(c == firstpic){
+                    let sibling = c.nextElementSibling;
+                    let sibling2 = secondpic.nextElementSibling;
+                    parent.insertBefore(firstpic, sibling2);
+                    parent.insertBefore(secondpic, sibling);
+                    break
+                }
+            }
+
+            firstpic.style.border = '';
+            firstpic = undefined;
+            echange.checked = false;
+        }
     }
 }
