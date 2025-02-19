d4d6415601666b1f8caf1a5be344942f63e0526c
Kylian2 <kylian.richard@gmail.com>
2025-02-19 11:54:31 +0100

    TP2: fin ex2

--- TP2/ex2/script/tableau.js
+++ TP2/ex2/script/tableau.js
@@ -1,3 +1,6 @@
+let data;
+let table;
+
 document.addEventListener("DOMContentLoaded", (event) => {
     
     table = [];
@@ -43,4 +46,138 @@ document.addEventListener("DOMContentLoaded", (event) => {
         g.textContent = (x/n).toFixed(2);
         i += n;
     })
+
+    /* Création d'un json comportant les données du tableau */
+    const namesDOM = document.querySelectorAll('tbody > tr > th');
+    const names = Array.from(namesDOM).map(n => n.textContent);
+
+    const libellesDOM = document.querySelectorAll('thead > tr:nth-child(2) > th');
+    const libelles = Array.from(libellesDOM).map(n => n.textContent);
+
+    data = {
+        libelles : libelles,
+        means: means,
+        students : []
+    };
+
+    for(let i = 0; i < names.length; i++){
+
+        infos = {
+            name: names[i],
+            grades: table[i],
+        }
+
+        data['students'].push(infos);
+    }
+
+    console.log(data);
 })
+
+function desktop_to_mobile(){
+    const table = document.getElementById('table');
+    console.log(table);
+    table.classList.toggle('mobile');
+
+    /* Vide le tableau */
+    while(table.firstChild){
+        table.removeChild(table.firstChild);
+    }
+    
+    /* Construit le thead */
+    const thead = document.createElement('thead');
+    const tr = document.createElement('tr');
+    
+    const th1 = document.createElement('th');
+    th1.textContent = "Discipline";
+    
+    const th2 = document.createElement('th');
+    th2.textContent = "Note";
+    
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
+    const tbody = document.createElement('tbody');
+    table.appendChild(tbody);
+
+    /* Construit le contenu */
+    data['students'].forEach((s, j) => {
+        const tr = document.createElement('tr');
+        const td = document.createElement('td');
+        td.setAttribute('colspan', 2);
+        td.textContent = s['name'];
+        tr.appendChild(td);
+        tbody.appendChild(tr);
+        s['grades'].forEach((grade, i) => {
+            const tr = document.createElement('tr');
+            if ((j+1) % 2 === 0) {
+                tr.style.filter = "invert(0.1)"; // Exemple de style
+            }
+            const th = document.createElement('th');
+            th.textContent = data['libelles'][i];
+            tr.appendChild(th);
+            const td = document.createElement('td');
+            td.textContent = grade;
+            if(grade < 10){
+                td.style.border = '2px solid red';
+            }
+            tr.appendChild(td);
+            tbody.appendChild(tr);
+        })
+    })
+}
+
+function mobile_to_desktop(){
+    const table = document.getElementById('table');
+    console.log(table);
+    table.classList.toggle('mobile');
+
+    /* Vide le tableau */
+    while(table.firstChild){
+        table.removeChild(table.firstChild);
+    }
+
+    /* Construit le thead */
+    const thead = document.createElement('thead');
+    thead.appendChild(document.createElement('td'));
+    data['libelles'].forEach(libelle => {
+        const th = document.createElement('th');
+        th.textContent = libelle;
+        thead.appendChild(th);
+    })
+    table.appendChild(thead);
+
+    /* Construit le body */
+    const tbody = document.createElement('tbody');
+    data['students'].forEach(s => {
+        const tr = document.createElement('tr');
+        const th = document.createElement('th');
+        th.textContent = s['name'];
+        tr.appendChild(th);
+        s['grades'].forEach(grade => {
+            const td = document.createElement('td');
+            td.textContent = grade;
+            if(grade < 10){
+                td.style.border = '2px solid red';
+            }
+            tr.appendChild(td);
+        })
+        tbody.appendChild(tr);
+    })
+    table.appendChild(tbody);
+
+    /*Construction des moyennes et agrégations */
+    const tfoot = document.createElement('tfoot');
+    const tr = document.createElement('tr');
+    const th = document.createElement('th');
+    th.textContent = "Moyenne";
+    tr.appendChild(th);
+    data['means'].forEach(mean => {
+        const td = document.createElement('td');
+        td.textContent = mean;
+        tr.appendChild(td);
+    });
+    tfoot.appendChild(tr);
+    table.appendChild(tfoot);
+}
