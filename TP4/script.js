c3c18586d605a4db4d85a1d73fa0c28c5be07a86
Kylian2 <kylian.richard@gmail.com>
2025-03-26 11:33:59 +0100

    TP4: pendu

--- TP4/script.js
+++ TP4/script.js
@@ -1,3 +1,8 @@
+let word = [];
+let errors = 0;
+let body_element = [];
+let found_letters = [];
+
 const fetch_word = async () => {
     try{
         const min_input = document.getElementById("min_char");
@@ -23,11 +28,9 @@ const fetch_word = async () => {
 }   
 
 const start_game = async () =>{
-    const word = await fetch_word();
-    if(word){
-        console.log(word);
-    }else{
-        return;
+    word = await fetch_word();
+    if(!word){
+        return
     }
 
     const init = document.getElementById("init");
@@ -36,10 +39,92 @@ const start_game = async () =>{
     const game = document.getElementById("game");
     game.classList.remove("none");
 
+    found_letters = [];
+    errors = 0;
+
     const guess_zone = document.getElementById("guess-zone");
+
+    while(guess_zone.firstChild){
+        guess_zone.removeChild(guess_zone.firstChild);
+    }
+
     for(let i = 0; i < word.length; i++){
         const span = document.createElement("span");
         span.classList.add("to-be-guessed");
         guess_zone.appendChild(span);
+        found_letters.push(false);
     }
+
+    document.addEventListener("keydown", checkkey);
+
+    body_element.push(document.getElementById("head"));
+    body_element.push(document.getElementById("body"));
+    body_element.push(document.getElementById("left-arm"));
+    body_element.push(document.getElementById("right-arm"));
+    body_element.push(document.getElementById("left-leg"));
+    body_element.push(document.getElementById("right-leg"));
+}
+
+const checkkey = (event)=>{
+    const key = (event.key).toUpperCase();
+    const guess_zone = document.getElementById('guess-zone');
+    const to_be_guessed = guess_zone.children;
+    let letter_is_found = false;
+    for(let i = 0; i < to_be_guessed.length; i++){
+        if(word[i] === key){
+            to_be_guessed[i].textContent = word[i];
+            to_be_guessed[i].classList.remove('to-be-guessed');
+            to_be_guessed[i].classList.add('found');
+            found_letters[i] = true;
+            letter_is_found = true;
+        }
+    }
+
+    let regex = /^[A-Z]$/;
+    if(!letter_is_found && regex.test(key)){
+        updateError();
+    }else{
+        if(found_letters.reduce((a, v) => a = a && v)){
+            victory();
+        }
+    }
+}
+
+const updateError = () => {
+    body_element[errors].style.display = 'block';
+    errors ++;
+    if(errors >= 6){
+        const game_over = document.getElementById("game-over");
+        const defeat = document.getElementById("game-defeat");
+        game_over.classList.remove('none');
+        defeat.classList.remove('none');
+        const reveal = document.querySelectorAll(".reveal");
+        reveal.forEach(r => {
+            r.textContent = word;
+        });
+    }
+}
+
+const victory = () => {
+    const game_over = document.getElementById("game-over");
+    const victory = document.getElementById("game-victory");
+    game_over.classList.remove('none');
+    victory.classList.remove('none');
+    const reveal = document.querySelectorAll(".reveal");
+    reveal.forEach(r => {
+        r.textContent = word;
+    });
+    const errors_container = document.getElementById('errors');
+    errors_container.textContent = errors;
+}
+
+const playAgain = () => {
+    const game_over = document.getElementById("game-over");
+    const victory = document.getElementById("game-victory");
+    const game = document.getElementById("game");
+    const init = document.getElementById("init");
+    game_over.classList.add("none");
+    victory.classList.add("none");
+    game.classList.add("none");
+    init.classList.remove("none");
 }
